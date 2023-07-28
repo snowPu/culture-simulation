6006d1ea8d3827d02d2a3c06fac2984a5ed2a299
sukanya <sukanya.dasgupta@ablacon.com>
2023-07-28 22:04:24 +0200

    Initial bacteria setup.

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import * as p5 from 'p5';
+import { Bacterium, ecoli } from './types/Bacterium';
+
+const canvasSize = [800, 800]
+
+export const sketch = (p: p5) => {
+    let bacteria: Bacterium[] = []
+
+    p.setup = () => {
+        p.createCanvas(canvasSize[0], canvasSize[1]);
+        bacteria.push(ecoli(p, p.createVector(p.width / 2, p.height / 2)))
+    }
+
+    p.draw = () => {
+        p.background(100,100,100);
+        bacteria.forEach(bacterium => bacterium.draw())
+        bacteria.forEach(bacterium => bacterium.move())
+        bacteria.forEach(bacterium => bacterium.updateVelocity())
+        let prob1 = Math.random()
+        if (prob1 < 0.05 && bacteria.length < 500) {
+          for (const bacterium of bacteria) {
+            let prob2 = Math.random()
+            if (prob2 < 0.05) {
+              bacteria.push(bacterium.mitose())
+            }
+          }
+        }
+    }
+}
+
+export const myp5 = new p5(sketch, document.body);
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    port: 5000
-  }
-}
+    mode: "development",
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: './dist',
+    }
+};
