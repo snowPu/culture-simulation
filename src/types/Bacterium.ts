1b6874fbcf843479fdc6a9151e323adc33603252
sukanya <sukanya.dasgupta@ablacon.com>
2023-08-09 14:57:50 +0200

    Add Shape.

--- src/types/Bacterium.ts
+++ src/types/Bacterium.ts
@@ -1,4 +1,5 @@
 import * as p5 from 'p5';
+import { Oval, Shape } from './Shape';
 
 export class Bacterium {
     p: p5
@@ -6,17 +7,24 @@ export class Bacterium {
     metabolicPathways: MetabolicPathway[]
     // splits per time unit
     growthRate: number
-    size: p5.Vector
+    shape: Shape
     location: p5.Vector
     velocity: p5.Vector
     rotation: number
 
-    constructor(p: p5, motility: number, metabolicPathways: MetabolicPathway[], growthRate: number, size: p5.Vector, location: p5.Vector) {
+    constructor(
+        p: p5,
+        motility: number,
+        metabolicPathways: MetabolicPathway[],
+        growthRate: number,
+        shape: Shape,
+        location: p5.Vector
+    ) {
         this.p = p
         this.motility = motility
         this.metabolicPathways = metabolicPathways
         this.growthRate = growthRate
-        this.size = size
+        this.shape = shape
         this.location = location
         this.velocity = p.createVector(0, 0)
         this.rotation = 0
@@ -29,21 +37,57 @@ export class Bacterium {
             this.motility,
             this.metabolicPathways,
             this.growthRate,
-            this.size,
+            this.shape,
             this.p.createVector(this.location.x + step, this.location.y + step),
         )
     }
 
     public updateVelocity() {
-        if (this.location.x <= this.size.x || this.location.x >= this.p.width - this.size.x) {
+        const {
+            left,
+            top,
+            right,
+            bottom,
+        } = this.shape.getExtremePoints(this.location, this.rotation)
+        if (
+            top.x <= 0 ||
+            right.x <= 0 ||
+            bottom.x <= 0 ||
+            left.x <= 0
+        ) {
+            this.location.x = Math.max(top.x, right.x, bottom.x, left.x)
             this.velocity.x *= -1
         }
-        else if (this.location.y <= this.size.y || this.location.y >= this.p.height - this.size.y) {
+        else if (
+            top.x >= this.p.width ||
+            right.x >= this.p.width ||
+            bottom.x >= this.p.width ||
+            left.x >= this.p.width
+        ) {
+            this.location.x = Math.min(top.x, right.x, bottom.x, left.x)
+            this.velocity.x *= -1
+        }
+        else if (
+            top.y <= 0 ||
+            right.y <= 0 ||
+            bottom.y <= 0 ||
+            left.y <= 0
+        ) {
+            this.location.y = Math.max(top.y, right.y, bottom.y, left.y)
+            this.velocity.y *= -1
+        }
+        else if (
+            top.y >= this.p.height ||
+            right.y >= this.p.height ||
+            bottom.y >= this.p.height ||
+            left.y >= this.p.height
+        ) {
+            this.location.y = Math.min(top.y, right.y, bottom.y, left.y)
             this.velocity.y *= -1
         }
         else {
-            let stepX = (Math.random() - 0.5) * .02
-            let stepY = (Math.random() - 0.5) * .02
+            let stepX = (Math.random() - 0.5) * .05
+            let stepY = (Math.random() - 0.5) * .05
             this.velocity.x += Math.min(stepX, 1.0)
             this.velocity.y += Math.min(stepY, 1.0)
         }
@@ -65,7 +109,7 @@ export class Bacterium {
         this.p.fill(200, 200, 200)
         this.p.translate(this.location.x, this.location.y)
         this.p.rotate(this.rotation)
-        this.p.ellipse(0, 0, this.size.x, this.size.y)
+        this.shape.draw()
         this.p.pop()
     }
 }
@@ -75,6 +119,6 @@ export const ecoli = (p: p5, location: p5.Vector) => new Bacterium(
     10,
     [{ input: ['Glucose', 'Oxygen'], output: ['CarbonDiOxide', 'Water'] }],
     5,
-    p.createVector(15, 10),
+    new Oval(p, p.createVector(7, 5)),
     location,
 )
---
+++ src/types/Shape.ts
@@ -0,0 +1,50 @@
+export interface Shape {
+    p: p5
+    /*
+    * Returns the four extreme points of a shape.
+    */
+    getExtremePoints(location: p5.Vector, rotation: number): ExtremePoints;
+
+    /* 
+    * Draws the shape.
+    */
+    draw(): void;
+}
+
+export class Oval implements Shape {
+    p: p5
+    radius: p5.Vector
+
+    constructor(p: p5, radius: p5.Vector) {
+        this.p = p
+        this.radius = radius
+    }
+
+    public getExtremePoints(location: p5.Vector, rotation: number): ExtremePoints {
+        const left = this.p.createVector(
+            location.x + this.radius.y * Math.sin(rotation),
+            location.y - this.radius.y * Math.cos(rotation),
+        )
+        const top = this.p.createVector(
+            location.x + this.radius.x * Math.cos(rotation),
+            location.y + this.radius.x * Math.sin(rotation),
+        )
+        const right = this.p.createVector(
+            location.x - this.radius.y * Math.sin(rotation),
+            location.y + this.radius.y * Math.cos(rotation),
+        )
+        const bottom = this.p.createVector(
+            location.x - this.radius.x * Math.cos(rotation),
+            location.y - this.radius.x * Math.sin(rotation),
+        )
+        return {
+            left,
+            top,
+            right,
+            bottom,
+        }
+    }
+    public draw() {
+        this.p.ellipse(0, 0, this.radius.x * 2, this.radius.y * 2)
+    }
+}
