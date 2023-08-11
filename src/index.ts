2dc63598b498c70ae1f5482556760b77ca36d3eb
nilshoffard <dr.rock1312@gmail.com>
2023-08-11 09:18:36 +0200

    Split/Time

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ export const sketch = (p: p5) => {
     let bacteria: Bacterium[] = []
 
     p.setup = () => {
-        console.log("Hi this is Nils")
         p.createCanvas(canvasSize[0], canvasSize[1]);
         bacteria.push(ecoli(p, p.createVector(p.width / 2, p.height / 2)))
     }
@@ -18,13 +17,14 @@ export const sketch = (p: p5) => {
         bacteria.forEach(bacterium => bacterium.move())
         bacteria.forEach(bacterium => bacterium.updateVelocity())
         let prob1 = Math.random()
-        if (prob1 < 0.05 && bacteria.length < 500) {
+        if (bacteria.length < 500) {
+          const newBacteria: Bacterium[] = []
           for (const bacterium of bacteria) {
-            let prob2 = Math.random()
-            if (prob2 < 0.05) {
-              bacteria.push(bacterium.mitose())
+            if (p.frameCount % bacterium.splitAt == 0 ) {
+              newBacteria.push(bacterium.mitose())
             }
           }
+          bacteria.push(...newBacteria)
         }
     }
 }
